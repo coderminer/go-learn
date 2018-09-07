9918165c4448914ef9a54dff75f59e878352d42d
kevin <kevin@puhanda.com>
2018-09-07 16:28:04 +0800

    improve the example with sync.WaitGroup

--- goroutines/ex1/main.go
+++ goroutines/ex1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -11,15 +12,18 @@ type Job struct {
 	text string
 }
 
-func outputText(j *Job) {
+func outputText(j *Job, goGroup *sync.WaitGroup) {
 	for j.i < j.max {
 		time.Sleep(1 * time.Second)
 		fmt.Println(j.text)
 		j.i++
 	}
+	goGroup.Done()
 }
 
 func main() {
+	goGroup := new(sync.WaitGroup)
+	fmt.Println("Starting")
 	hello := new(Job)
 	world := new(Job)
 
@@ -31,7 +35,9 @@ func main() {
 	world.i = 0
 	world.max = 5
 
-	go outputText(hello)
-	outputText(world)
+	go outputText(hello, goGroup)
+	go outputText(world, goGroup)
 
+	goGroup.Add(2)
+	goGroup.Wait()
 }
